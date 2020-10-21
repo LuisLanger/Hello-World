a3b13c92a932376bdb2246b513522b495aa8a52e
LuisLanger <68463762+LuisLanger@users.noreply.github.com>
2020-10-21 13:46:39 +0200

    Create HelloWord.java

---
+++ HelloWord.java
@@ -0,0 +1,13 @@
+/*Created on 21/10/2020 13:37:47 UTC*/
+		/*JAVA*/
+//Imprimir a mensagem, "Hello World"[PT-pt]
+
+//Print message "Hello world"[EN]
+
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		System.out.print("Hello World");
+	}	
+}//end of class HelloWorld
